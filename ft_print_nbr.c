399a4c4f987cac6760f3a5c114c76d94e4315cb7
0rph3us <64805107+shadythegiant@users.noreply.github.com>
2025-11-09 12:51:36 +0000

    getting there

---
+++ ft_print_nbr.c
@@ -0,0 +1,45 @@
+#include <stdlib.h>
+#include <unistd.h>
+
+
+
+int ft_print_hex(unsigned long n , int uppercase) { 
+    char *hex; 
+    
+    hex = "123456789abcdef"; 
+    if(uppercase == 1) 
+        hex = "123456789ABCDEF"; 
+    if(n > 15) 
+       return(ft_print_hex(n / 16, uppercase ) + ft_putchar(hex[n % 16])); 
+     else 
+        return (ft_putchar(hex[n]));   
+
+}
+
+
+
+int ft_putunbr(unsigned int n) {
+
+    if(n > 9)
+        return (ft_putunbr(n / 10) + ft_putchar((n % 10) + '0')); 
+     else 
+        return(ft_putchar(n + '0')); 
+}
+
+int ft_putnbr(int n) { 
+    long nbr; 
+    int count; 
+
+    nbr = n; 
+    count = 0; 
+    if(nbr < 0) { 
+        count += ft_putchar('-'); 
+        nbr = -nbr; 
+    }
+    if(nbr > 9) {
+        count += ft_putnbr(nbr / 10); 
+        return(count + ft_putchar((nbr % 10) + '0')); 
+    } 
+    else 
+     return( count + ft_putchar(nbr + '0')); 
+}
